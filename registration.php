c44384f30b23356f97089fac96ffb53ff65f9f5d
DVasthav <143284720+DVasthav@users.noreply.github.com>
2024-04-20 11:50:44 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -79,7 +79,8 @@ if(isset($_REQUEST['username'])){
     </div>";
 }
 else{
- $query = "INSERT into `users`(name, email, mobileno,username,password)
+    echo "I am calling 3";
+ $query = "INSERT into heroku_0a22cdb987db64a.users (name, email, mobileno,username,password)
             VALUES ('$name','$email','$mobileno','$username','$password')";
  $result =mysqli_query($db_con, $query);
  if ($result){
